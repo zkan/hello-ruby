122297ca7e6983b0cd6602a4d62b222d1f5f8cf2
zkan <kan@odds.team>
2025-06-17 13:54:37 +0700

    Test delicious and size

---
+++ lobster_roll/lobster_roll.rb
@@ -0,0 +1,27 @@
+require "minitest"
+
+class LobsterRoll
+  def size
+    :big
+  end
+
+  def delicious?
+    true
+  end
+end
+
+class LobsterRollTest < Minitest::Test
+  def setup
+    @roll = LobsterRoll.new
+  end
+
+  def test_is_delicious
+    assert @roll.delicious?
+  end
+
+  def test_size
+    assert_equal :big, @roll.size
+  end
+end
+
+Minitest.run(ARGV)
