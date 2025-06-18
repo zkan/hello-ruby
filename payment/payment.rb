6fdb764b782a9df9b6ae96a3a82afb94cd73f35f
zkan <kan@odds.team>
2025-06-18 16:42:11 +0700

    Stub the provider and time

--- payment/payment.rb
+++ payment/payment.rb
@@ -4,6 +4,11 @@ class StripProvider
 end
 
 class User
+  attr_accessor :balance, :last_transaction_at
+
+  def initialize(balance: 0)
+    @balance = balance
+  end
 end
 
 class Payment
@@ -13,5 +18,14 @@ class Payment
 
   def charge(user, amount)
     @provider.charge(user, amount)
+  end
+end
+
+class Subscription
+  def self.charge(user, amount)
+    provider = StripProvider.new
+    provider.charge(user, amount)
+    user.balance = (user.balance - amount)
+    user.last_transaction_at = Time.now
   end
 end
--- payment/test/payment_test.rb
+++ payment/test/payment_test.rb
@@ -13,3 +13,22 @@ class PaymentTest < Minitest::Test
     assert_mock @mock_provider
   end
 end
+
+class SubscriptionTest < Minitest::Test
+  def setup
+    @mock_provider = Minitest::Mock.new
+    @mock_provider.expect(:charge, true, [Object, Numeric])
+  end
+
+  def test_charge_adjusts_balance
+    StripProvider.stub :new, @mock_provider do
+      Time.stub(:now, Time.new(2024, 1, 1)) do
+        user = User.new(balance: 300)
+        assert Subscription.charge(user, 100)
+        assert_equal 200, user.balance
+        assert_equal Time.new(2024, 1, 1).to_i, user.last_transaction_at.to_i
+      end
+    end
+    assert_mock @mock_provider
+  end
+end
